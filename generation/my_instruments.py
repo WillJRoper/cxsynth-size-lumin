ba5194b1bfd1b8e06ca9ed7e0d0f0b7da19c01f0
wjr21 <wjr21@sussex.ac.uk>
2025-04-01 16:31:26 +0100

    Clean up instruments

--- generation/my_instruments.py
+++ generation/my_instruments.py
@@ -6,36 +6,6 @@ from synthesizer.instruments import FilterCollection
 from synthesizer.instruments.instrument import Instrument
 from unyt import angstrom, arcsecond, kpc
 
-# Define the filters
-nircam_fs = FilterCollection(
-    filter_codes=[
-        "JWST/NIRCam.F090W",
-        "JWST/NIRCam.F115W",
-        "JWST/NIRCam.F150W",
-        "JWST/NIRCam.F200W",
-        "JWST/NIRCam.F277W",
-        "JWST/NIRCam.F356W",
-        "JWST/NIRCam.F444W",
-    ]
-)
-
-miri_fs = FilterCollection(
-    filter_codes=[
-        "JWST/MIRI.F560W",
-        "JWST/MIRI.F770W",
-        "JWST/MIRI.F1000W",
-        "JWST/MIRI.F1130W",
-        "JWST/MIRI.F1280W",
-        "JWST/MIRI.F1500W",
-        "JWST/MIRI.F1800W",
-    ]
-)
-top_hat = FilterCollection(
-    tophat_dict={
-        "UV1500": {"lam_eff": 1500 * angstrom, "lam_fwhm": 300 * angstrom},
-    },
-)
-
 
 def make_instruments(inst_path, z):
     """
@@ -45,6 +15,36 @@ def make_instruments(inst_path, z):
         inst_path (str): The path to save the instruments.
         z (float): The redshift of the galaxies.
     """
+    # Define the filters
+    nircam_fs = FilterCollection(
+        filter_codes=[
+            "JWST/NIRCam.F090W",
+            "JWST/NIRCam.F115W",
+            "JWST/NIRCam.F150W",
+            "JWST/NIRCam.F200W",
+            "JWST/NIRCam.F277W",
+            "JWST/NIRCam.F356W",
+            "JWST/NIRCam.F444W",
+        ]
+    )
+
+    miri_fs = FilterCollection(
+        filter_codes=[
+            "JWST/MIRI.F560W",
+            "JWST/MIRI.F770W",
+            "JWST/MIRI.F1000W",
+            "JWST/MIRI.F1130W",
+            "JWST/MIRI.F1280W",
+            "JWST/MIRI.F1500W",
+            "JWST/MIRI.F1800W",
+        ]
+    )
+    top_hat = FilterCollection(
+        tophat_dict={
+            "UV1500": {"lam_eff": 1500 * angstrom, "lam_fwhm": 300 * angstrom},
+        },
+    )
+
     # Set up the PSF dictionaries and webbpsf objects
     nircam_psfs = {}
     miri_psfs = {}
@@ -55,13 +55,13 @@ def make_instruments(inst_path, z):
     for nc_filt in nircam_fs.filter_codes:
         nc.filter = nc_filt.split(".")[-1]
         psf = nc.calc_psf(oversample=2)
-        nircam_psfs[nc_filt.replace("/", "_")] = psf[0].data
+        nircam_psfs[nc_filt] = psf[0].data
 
     # Get miri psfs
     for miri_filt in miri_fs.filter_codes:
         miri.filter = miri_filt.split(".")[-1]
         psf = miri.calc_psf(oversample=2)
-        miri_psfs[miri_filt.replace("/", "_")] = psf[0].data
+        miri_psfs[miri_filt] = psf[0].data
 
     # Define the angular resoltions
     nircam_res = 0.031 * arcsecond

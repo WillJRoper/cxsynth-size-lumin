6dc5bb77800d7bd92e4ce2b13ba03b7580a103e4
wjr21 <wjr21@sussex.ac.uk>
2025-04-01 14:39:45 +0100

    Debugging

--- generation/colibre_data_loader.py
+++ generation/colibre_data_loader.py
@@ -136,6 +136,14 @@ def _get_galaxies(
         star_mask = star_radii <= aperture
         gas_mask = gas_radii <= aperture
 
+        print(
+            gal_ind,
+            np.sum(star_mask),
+            np.sum(gas_mask),
+            stars_coords.shape,
+            gas_coords.shape,
+        )
+
         # Derive the ages from the scale_factors
         scale_factors = swift_gal.stars.birth_scale_factors.to_value()
         scale_factors[scale_factors > aexp] = aexp

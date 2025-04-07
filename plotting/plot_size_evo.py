771e11355e30257e569ffd28ca0edcb30988cf61
wjr21 <wjr21@sussex.ac.uk>
2025-04-07 18:56:08 +0100

    Implemented fitting of size evolution

--- plotting/plot_size_evo.py
+++ plotting/plot_size_evo.py
@@ -4,6 +4,12 @@ import argparse
 import glob
 import os
 
+import h5py
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import curve_fit
+from scipy.stars import binned_statistic
+
 
 def plot_size_evolution(filepath):
     """
@@ -21,8 +27,92 @@ def plot_size_evolution(filepath):
     lengths = [len(f.split("_")) for f in files]
     combined_lengths = min(lengths)
     files = [f for f in files if len(f.split("_")) == combined_lengths]
+
     print(f"Found {len(files)} files.")
     print(f"Files: {files}")
+
+    # Loop over the files and extract the redshifts and sizes
+    redshifts = []
+    sizes = []
+    for f in files:
+        # Open the file and extract the sizes and redshifts
+        with h5py.File(f, "r") as hdf:
+            # Get the redshift (it's the same for all galaxies)
+            redshift = hdf["Galaxies/Redshift"][0]
+            sizes.extend(hdf["Galaxies/Stars/MassRadii/0p5"][...])
+            redshifts.extend(redshift)
+
+    # Convert the data to numpy arrays
+    redshifts = np.array(redshifts)
+    sizes = np.array(sizes)
+
+    # Convert the sizes to kpc
+    sizes = sizes * 1e3
+
+    # Fit a line to the data
+    popt, pcovr = curve_fit(lambda z, a, b: a * (1 + z) ** b, redshifts, sizes)
+    fit_xs = np.linspace(min(redshifts), max(redshifts), 100)
+    fit_ys = popt[0] * (1 + fit_xs) ** popt[1]
+
+    # Compute the median in each redshift bin and the standard deviation in
+    # each bin so we can plot the error bars
+    median_xs = np.logspace(
+        np.log10(np.min(redshifts)),
+        np.log10(np.max(redshifts)),
+        15,
+    )
+    median_ys = binned_statistic(
+        redshifts,
+        sizes,
+        statistic="median",
+        bins=median_xs,
+    )[0]
+
+    median_ys_std = binned_statistic(
+        redshifts,
+        sizes,
+        statistic="std",
+        bins=median_xs,
+    )[0]
+
+    print(f"Fitted parameters: {popt}")
+    print(f"Fitted covariance: {pcovr}")
+
+    # Plot the data
+    fig, ax = plt.subplots(figsize=(3.5, 3.5))
+
+    # Plot the binned data
+    ax.errorbar(
+        (median_xs[:-1] + median_xs[1:]) / 2,
+        median_ys,
+        yerr=median_ys_std,
+        fmt="o",
+        color="k",
+    )
+
+    # Plot the fit
+    ax.plot(
+        fit_xs,
+        fit_ys,
+        color="r",
+        linestyle="--",
+        label="Fit",
+    )
+
+    # Set the axis labels
+    ax.set_xlabel(r"$z$")
+    ax.set_ylabel(r"$R_{1/2} / [\mathrm{kpc}]$")
+
+    ax.legend(
+        loc="upper left",
+        fontsize=8,
+    )
+
+    fig.savefig(
+        f"{outpath}/size_evolution.png",
+        dpi=300,
+        bbox_inches="tight",
+    )
 
 
 if __name__ == "__main__":

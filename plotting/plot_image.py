ebf92c4103a9035a5b81a67c5aa893ebcb976d0a
wjr21 <wjr21@sussex.ac.uk>
2025-04-09 11:34:24 +0200

    percentiles for rgb image

--- plotting/plot_image.py
+++ plotting/plot_image.py
@@ -177,6 +177,8 @@ def plot_rgb_image(path, outpath, run_name, variant, snap, gal_ind=None):
     )
     fig, ax, _ = img_coll.plot_rgb_image(
         show=False,
+        vmax=np.percentile(img_coll.rgb_img, 99.9),
+        vmin=np.percentile(img_coll.rgb_img, 16),
     )
 
     # Include the redshift in the title

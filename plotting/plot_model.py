077c7822d17547d2e7da2b1f83e33ee187123eb6
wjr21 <wjr21@sussex.ac.uk>
2025-04-10 00:59:38 +0200

    Forget relative import

--- plotting/plot_model.py
+++ plotting/plot_model.py
@@ -2,16 +2,133 @@
 
 import warnings
 
+from synthesizer.emission_models import (
+    EmissionModel,
+    NebularEmission,
+    ReprocessedEmission,
+    StellarEmissionModel,
+    TransmittedEmission,
+)
+from synthesizer.emission_models.transformers import PowerLaw
 from synthesizer.grid import Grid
-from unyt import Msun, angstrom
-
-from ..generation.my_emission_models import LOSStellarEmission
+from unyt import Msun, Myr, angstrom
 
 # Silence warnings (only because we now what we're doing)
 warnings.filterwarnings("ignore")
 
 # Msun needs to be respected
 Msun = Msun.in_base("galactic")
+
+
+class LOSStellarEmission(EmissionModel):
+    """
+    The stellar emission model used for in FLARES.
+
+    This model is a subclass of the StellarEmissionModel class and is used
+    to generate the stellar emission for galaxies in FLARES.
+    """
+
+    def __init__(self, grid):
+        """
+        Initialize the FLARESLOSEmission model.
+
+        Args:
+            grid (Grid): The grid to use for the model.
+        """
+        # Define the nebular emission models
+        nebular = NebularEmission(
+            grid=grid,
+            label="nebular",
+            mask_attr="ages",
+            mask_op="<=",
+            mask_thresh=10 * Myr,
+        )
+
+        # Define the transmitted models
+        young_transmitted = TransmittedEmission(
+            grid=grid,
+            label="young_transmitted",
+            mask_attr="ages",
+            mask_op="<=",
+            mask_thresh=10 * Myr,
+        )
+        old_transmitted = TransmittedEmission(
+            grid=grid,
+            label="old_transmitted",
+            mask_attr="ages",
+            mask_op=">",
+            mask_thresh=10 * Myr,
+        )
+        transmitted = StellarEmissionModel(
+            grid=grid,
+            label="transmitted",
+            combine=[young_transmitted, old_transmitted],
+        )
+
+        # Define the reprocessed models
+        young_reprocessed = ReprocessedEmission(
+            grid=grid,
+            label="young_reprocessed",
+            transmitted=young_transmitted,
+            nebular=nebular,
+            mask_attr="ages",
+            mask_op="<=",
+            mask_thresh=10 * Myr,
+        )
+        reprocessed = StellarEmissionModel(
+            grid=grid,
+            label="reprocessed",
+            combine=[young_reprocessed, old_transmitted],
+        )
+
+        # Define the attenuated models
+        young_attenuated_nebular = StellarEmissionModel(
+            grid=grid,
+            label="young_attenuated_nebular",
+            apply_dust_to=young_reprocessed,
+            tau_v="young_tau_v",
+            dust_curve=PowerLaw(slope=-1),
+            mask_attr="ages",
+            mask_op="<=",
+            mask_thresh=10 * Myr,
+        )
+        young_attenuated = StellarEmissionModel(
+            grid=grid,
+            label="young_attenuated",
+            apply_dust_to=young_attenuated_nebular,
+            tau_v="tau_v",
+            dust_curve=PowerLaw(slope=-1),
+            mask_attr="ages",
+            mask_op="<=",
+            mask_thresh=10 * Myr,
+        )
+        old_attenuated = StellarEmissionModel(
+            grid=grid,
+            label="old_attenuated",
+            apply_dust_to=old_transmitted,
+            tau_v="tau_v",
+            dust_curve=PowerLaw(slope=-1),
+            mask_attr="ages",
+            mask_op=">",
+            mask_thresh=10 * Myr,
+        )
+
+        # Finaly, combine to get the emergent emission
+        EmissionModel.__init__(
+            self,
+            grid=grid,
+            label="stellar_total",
+            combine=[young_attenuated, old_attenuated],
+            related_models=[
+                nebular,
+                transmitted,
+                reprocessed,
+                young_attenuated_nebular,
+            ],
+            emitter="stellar",
+        )
+
+        self.set_per_particle(True)
 
 
 def get_emission_model(

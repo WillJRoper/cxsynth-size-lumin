2678a6301af29746d1cee52f83dfaa546060deb5
wjr21 <wjr21@sussex.ac.uk>
2025-04-10 14:51:38 +0200

    Log scaling the right plot this time

--- plotting/plot_size_lumin.py
+++ plotting/plot_size_lumin.py
@@ -328,6 +328,7 @@ def plot_size_lum_hex_fit_multi(
     if fig is None:
         fig = plt.figure(figsize=(3.5, 3.5))
         ax = fig.add_subplot(111)
+        ax.loglog()
 
         # Add a grid and make sure its always at the back
         ax.grid(True)
